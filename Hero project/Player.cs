a2cdc481965622b09993e8346b2c3a81198e5446
Ostap4i <137522973+Ostap4i@users.noreply.github.com>
2024-01-22 16:08:08 +0200

    Add files via upload

---
+++ Hero project/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hero_project
+{
+    internal class Player
+    {
+
+        public void ChooseHero()
+        {
+            Console.WriteLine("Choose your hero:");
+            Console.WriteLine("1. Warrior");
+            Console.WriteLine("2. Mage");
+            Console.WriteLine("3. Archer");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+    }
+}
